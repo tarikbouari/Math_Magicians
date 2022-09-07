ada69437904a149b4150f5bd358d13d63787a260
Rikstone <tarikbouari@gmail.com>
2022-09-07 10:54:36 +0000

    Create calculator component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,18 +1,13 @@
+import React from 'react';
+import Table from './Table';
 
-class Calculator extends React.Component {
-    constructor (props){
-        super(props)
-    }
+export default class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-
-        )
-    }
-}
-
-const table = (props) => {
-    return {
-
-    }
+  render() {
+    return <Table />;
+  }
 }
