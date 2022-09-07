c0974c8ed8b238f52f4c6f5df0f89d2786e85dfe
Rikstone <tarikbouari@gmail.com>
2022-09-07 05:23:05 +0000

    Create componebt directory

---
+++ src/components/calculator.js
@@ -0,0 +1,18 @@
+
+class Calculator extends React.Component {
+    constructor (props){
+        super(props)
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+const table = (props) => {
+    return {
+
+    }
+}
