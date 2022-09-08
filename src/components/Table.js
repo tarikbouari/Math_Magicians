8843991dca033205a173c3d5845c892a2454f281
Rikstone <tarikbouari@gmail.com>
2022-09-08 13:53:11 +0000

    add event click function

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,63 @@
-const Table = () => (
-  <div className="container">
-    <div className="render">
-      <p>0</p>
-    </div>
-    <div className="row1">
-      <input type="button" className="ac" value="AC" />
-      <input type="button" className="ac" value="+/-" />
-      <input type="button" className="ac" value="%" />
-      <input type="button" className="ac" value="÷" />
-    </div>
+import React from 'react';
+import calculate from '../logic/calculate';
 
-    <div className="row2">
-      <input type="button" className="ac" value="7" />
-      <input type="button" className="plus" value="8" />
-      <input type="button" className="perc" value="9" />
-      <input type="button" className="div" value="X" />
-    </div>
-    <div className="row3">
-      <input type="button" className="four" value="4" />
-      <input type="button" className="five" value="5" />
-      <input type="button" className="six" value="6" />
-      <input type="button" className="moins" value="-" />
-    </div>
+class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: '', next: null, operation: null };
+    this.clikHandler = this.clikHandler.bind(this);
+  }
 
-    <div className="row4">
-      <input type="button" className="one" value="1" />
-      <input type="button" className="two" value="2" />
-      <input type="button" className="three" value="3" />
-      <input type="button" className="four" value="+" />
-    </div>
-    <div className="row5">
-      <input type="button" className="zero" value="0" />
-      <input type="button" className="point" value="." />
-      <input type="button" className="equal" value="=" />
-    </div>
-  </div>
-);
+  clikHandler(e) {
+    const { value } = e.target;
+    const data = calculate(this.state, value);
+    this.setState(data);
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+
+      <div className="container">
+        <div className="render">
+          {next}
+          {operation}
+          {total}
+        </div>
+        <div className="row1">
+          <input type="button" onClick={this.clikHandler} className="ac" value="AC" />
+          <input type="button" onClick={this.clikHandler} className="ac" value="+/-" />
+          <input type="button" onClick={this.clikHandler} className="ac" value="%" />
+          <input type="button" onClick={this.clikHandler} className="ac" value="÷" />
+        </div>
+
+        <div className="row2">
+          <input type="button" onClick={this.clikHandler} className="ac" value="7" />
+          <input type="button" onClick={this.clikHandler} className="plus" value="8" />
+          <input type="button" onClick={this.clikHandler} className="perc" value="9" />
+          <input type="button" onClick={this.clikHandler} className="div" value="x" />
+        </div>
+        <div className="row3">
+          <input type="button" onClick={this.clikHandler} className="four" value="4" />
+          <input type="button" onClick={this.clikHandler} className="five" value="5" />
+          <input type="button" onClick={this.clikHandler} className="six" value="6" />
+          <input type="button" onClick={this.clikHandler} className="moins" value="-" />
+        </div>
+
+        <div className="row4">
+          <input type="button" onClick={this.clikHandler} className="one" value="1" />
+          <input type="button" onClick={this.clikHandler} className="two" value="2" />
+          <input type="button" onClick={this.clikHandler} className="three" value="3" />
+          <input type="button" onClick={this.clikHandler} className="four" value="+" />
+        </div>
+        <div className="row5">
+          <input type="button" onClick={this.clikHandler} className="zero" value="0" />
+          <input type="button" onClick={this.clikHandler} className="point" value="." />
+          <input type="button" onClick={this.clikHandler} className="equal" value="=" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Table;
