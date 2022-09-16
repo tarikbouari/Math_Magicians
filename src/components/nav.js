5f0a8aa31f5f2457f2e3f0e19e8b4abe057bb1fd
Rikstone <tarikbouari@gmail.com>
2022-09-16 03:00:07 +0000

    import Route library

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './nav.module.css';
 
-
-
-const Nav = () => {
-    return (
-        <div className={styles.containerNav}>
-            <nav className={styles.navbar}>
-                <a className={styles.logo}> Math Magicians </a>
-                <ul className={styles.boxlist}>
-                    <li>Home</li>
-                    <li>Calculator</li>
-                    <li>Quotes</li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+const Nav = () => (
+  <div className={styles.containerNav}>
+    <nav className={styles.navbar}>
+      <span className={styles.logo}>
+        <Link to="/"> Math Magicians</Link>
+        {' '}
+      </span>
+      <ul className={styles.boxlist}>
+        <li>
+          {' '}
+          <NavLink to="/">Home </NavLink>
+          {' '}
+        </li>
+        <li>
+          {' '}
+          <NavLink to="calculator">Calculator </NavLink>
+        </li>
+        <li><NavLink to="quotes">Quotes </NavLink></li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default Nav;
