eae135fe9150d7930d4ec06fe6d3aec30b54eeaf
Rikstone <tarikbouari@gmail.com>
2022-09-16 02:59:06 +0000

    update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,20 @@
-import React from "react";
-import styles from "./home.module.css";
+import React from 'react';
 
-const Home = () => {
-  return (
-    <div
-      style={{
-        padding: "4rem 0",
-      }}
-    >
-      <p>
-        Minim ut magna nulla officia consectetur proident dolor ipsum. Pariatur
-        in deserunt magna cupidatat proident eiusmod ad. Eiusmod est in id
-        voluptate. Occaecat veniam cupidatat aliqua sint adipisicing in Lorem
-        irure dolor velit sunt aliquip cupidatat. Veniam irure pariatur esse
-        sint sint aute ut quis fugiat eu non. Excepteur ea veniam pariatur
-        cupidatat occaecat cupidatat ex ex proident est.
-      </p>
-    </div>
-  );
-};
+const Home = () => (
+  <div
+    style={{
+      padding: '4rem 0',
+    }}
+  >
+    <p>
+      Minim ut magna nulla officia consectetur proident dolor ipsum. Pariatur
+      in deserunt magna cupidatat proident eiusmod ad. Eiusmod est in id
+      voluptate. Occaecat veniam cupidatat aliqua sint adipisicing in Lorem
+      irure dolor velit sunt aliquip cupidatat. Veniam irure pariatur esse
+      sint sint aute ut quis fugiat eu non. Excepteur ea veniam pariatur
+      cupidatat occaecat cupidatat ex ex proident est.
+    </p>
+  </div>
+);
 
 export default Home;
