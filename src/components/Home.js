555b81317a9dbacf9d2df7170a0496a46e032c85
Rikstone <tarikbouari@gmail.com>
2022-09-16 02:24:41 +0000

    Create home component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        padding: "4rem 0",
+      }}
+    >
+      <p>
+        Minim ut magna nulla officia consectetur proident dolor ipsum. Pariatur
+        in deserunt magna cupidatat proident eiusmod ad. Eiusmod est in id
+        voluptate. Occaecat veniam cupidatat aliqua sint adipisicing in Lorem
+        irure dolor velit sunt aliquip cupidatat. Veniam irure pariatur esse
+        sint sint aute ut quis fugiat eu non. Excepteur ea veniam pariatur
+        cupidatat occaecat cupidatat ex ex proident est.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
